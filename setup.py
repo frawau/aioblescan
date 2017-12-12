ed6b344404cd5d3253a2146d91e768c612b06a8b
Francois Wautier <francois@wautier.eu>
2017-12-12 15:50:23 +0700

    Preparing for PyPi

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(name='aioblescan',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ])
