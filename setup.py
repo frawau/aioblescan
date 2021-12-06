3e1996c7c48c4f312b385e47a8e0546ba1aeec23
Francois Wautier <francois@wautier.eu>
2021-12-06 13:37:59 +0700

    Preparing release 0.2.10 with tag

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     entry_points={"console_scripts": ["aioblescan=aioblescan.__main__:main"]},
     zip_safe=False,
